4d7764ae797db69b09b0f73c97114a70aa1e4b3d
Anubhav-2004 <anubhavchoubey54321@gmail.com>
2026-03-20 12:15:49 +0530

    update scopes

--- src/lib/shopify.ts
+++ src/lib/shopify.ts
@@ -4,7 +4,7 @@ import { shopifyApi, ApiVersion, LogSeverity } from '@shopify/shopify-api'
 export const shopify = shopifyApi({
   apiKey: process.env.SHOPIFY_API_KEY!,
   apiSecretKey: process.env.SHOPIFY_API_SECRET!,
-  scopes: ['read_customers', 'write_customers', 'read_orders', 'read_products'],
+  scopes: ['read_customers', 'write_customers', 'read_orders', 'write_orders', 'read_products'],
   hostName: process.env.HOST!.replace(/https?:\/\//, ''),
   hostScheme: 'https',
   apiVersion: ApiVersion.January25,
